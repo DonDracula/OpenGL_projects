054f0499b9bc25fc768b276b02df0e1412319a51
Liuyanhao <DonDracula@users.noreply.github.com>
2015-05-20 21:59:49 +0900

    update a program which draw a red heart using freegut structure

---
+++ DrawHeart/FeelMyLove/FeelMyLove/heart.h
@@ -0,0 +1,16 @@
+#ifndef _HEART
+#define _HEART
+
+//include files
+#include <vector>
+#include <string>
+
+//OpenGL
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+//Vec3 and other function
+#include "rx_utility.h"
+
+using namespace std;
+#endif
